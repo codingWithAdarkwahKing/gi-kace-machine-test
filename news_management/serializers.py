6906913b233ae958669b658f42ebed32c06a90b9
codingWithAdarkwahKing <kingadarkwah4@gmail.com>
2024-11-13 13:02:24 +0000

    add gitignore

--- news_management/serializers.py
+++ news_management/serializers.py
@@ -1,25 +1,21 @@
 from rest_framework import serializers
 from .models import Staff, Tag, Category, Report, ReportDesk
 
-# Staff Serializer
 class StaffSerializer(serializers.ModelSerializer):
     class Meta:
         model = Staff
         fields = ['id', 'user', 'department', 'bio']
 
-# Tag Serializer
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ['id', 'name']
 
-# Category Serializer
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'parent_category']
 
-# Report Serializer
 class ReportSerializer(serializers.ModelSerializer):
     author = StaffSerializer(read_only=True)
     category = CategorySerializer(read_only=True)
@@ -29,7 +25,6 @@ class ReportSerializer(serializers.ModelSerializer):
         model = Report
         fields = ['id', 'title', 'description', 'content', 'created_at', 'updated_at', 'author', 'category', 'tags', 'geo_coordinates', 'is_published', 'attachments']
 
-# ReportDesk Serializer (to handle editorial workflow)
 class ReportDeskSerializer(serializers.ModelSerializer):
     report = ReportSerializer(read_only=True)
     desk_head = StaffSerializer(read_only=True)
